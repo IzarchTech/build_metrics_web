2c10034fcf8aa1831824bcd780fb44ceb9ba3bba
Chukwunwike Udezue <udezuec@protonmail.com>
2024-11-08 13:46:07 +0100

    Feat: Cart Away interface

---
+++ packages/core/src/interfaces/cart-away.ts
@@ -0,0 +1,13 @@
+/**
+ * Cart away interface
+ */
+interface CartAway {
+  /**
+   * Get volume of earth to be carted away
+   *
+   * @returns volume of cart away
+   */
+  getVolumeOfCartAway(): number;
+}
+
+export default CartAway;
