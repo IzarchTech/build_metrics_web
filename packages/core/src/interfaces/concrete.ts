7261129f6202888206f530a8a1286d3901a57f19
Chukwunwike Udezue <udezuec@protonmail.com>
2024-11-08 10:09:39 +0100

    Feat: concrete interface

---
+++ packages/core/src/interfaces/concrete.ts
@@ -0,0 +1,11 @@
+/**
+ * Concrete interface
+ */
+interface Concrete {
+  /**
+   * Get the volume of concrete required for construction
+   *
+   * @returns Volume of concrete
+   */
+  getVolumeOfConcrete(): number;
+}
