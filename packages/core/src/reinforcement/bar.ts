3703b51db297c316137c200ff59ad7ce4a477eeb
Chukwunwike Udezue <udezuec@protonmail.com>
2024-12-03 12:23:42 +0100

    Fix: Typo

--- packages/core/src/reinforcement/bar.ts
+++ packages/core/src/reinforcement/bar.ts
@@ -2,12 +2,13 @@
  * Bar size
  *
  * @remarks Unit: mm (millimeter)
- * @summary Extract from BS 4466
  */
 export type BarSize = 6 | 8 | 10 | 12 | 16 | 20 | 25 | 32 | 40;
 
 /**
  * Reinforcement bar
+ *
+ * @summary Extract from BS 4466
  */
 class Bar {
   /**
